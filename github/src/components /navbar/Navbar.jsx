35b5c3ad77e64e8449e152b66a6520f68a9ec237
ak220193 <mail2meak22frcrio@gmail.com>
2024-02-02 20:58:45 +0530

    navbar completed

--- github/src/components /navbar/Navbar.jsx
+++ github/src/components /navbar/Navbar.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import styles from "./Navbar.module.css";
+import Logo from "../../Assets/logo.png";
 
 const Navbar = () => {
   return (
     <>
       <div className={styles.Navbar}>
         <div className={styles.logo}>
-          <img src={logo} alt="logo" />
+          <img src={Logo} alt="logo" />
+        </div>
+        <div className={styles.button}>
+          <button> Home </button>
+          <button> All Users </button>
         </div>
       </div>
     </>
